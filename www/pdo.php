a85a461b5dcbaf625120608f858e136ba2b8db76
Özgür Görgülü <ozgurgorgulu54@gmail.com>
2021-05-26 15:08:17 +0300

    Added PDO example

---
+++ www/pdo.php
@@ -0,0 +1,14 @@
+<?php
+$host = "database";
+$db = "myDockerDatabase";
+$user = "docker";
+$password = "docker";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    echo "Successfully connected to the database. 👍";
+} catch(Exception $exception) {
+    echo "Could not connect to database ({$exception->getMessage()}) 😔";
+}
